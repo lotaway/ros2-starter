401969a07c39e6d1409966f9d879460a17372296
LOTAWAY\lotaway <576696294@qq.com>
2026-03-03 11:36:45 +0000

    Update init shell.

---
+++ init_project.sh
@@ -0,0 +1,4 @@
+ # /bin/bash
+ ros2 pkg create ros2-starter \
+  --build-type ament_cmake \
+  --dependencies rclcpp rclpy std_msgs
--- preinstall.sh
+++ preinstall.sh
@@ -1,1 +1,2 @@
+ # /bin/bash
 colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
