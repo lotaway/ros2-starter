8c2ebf21c7cbbcf64c4367687f3b29010d0e4088
lotaway <576696294@qq.com>
2026-03-11 15:02:18 +0800

    Add mamba dev environment.

---
+++ mamba.sh
@@ -0,0 +1,21 @@
+# Create a conda environment named ros2_humble with Python 3.10 (recommended for Humble)
+mamba create -n ros2_humble python=3.10
+# Activate the environment
+mamba activate ros2_humble
+
+# Install full desktop version (includes rviz2, demo nodes, etc.)
+mamba install -c conda-forge -c robostack-staging ros-humble-desktop-full
+
+# Install build tools (for compiling packages)
+mamba install -c conda-forge -c robostack-staging compilers cmake pkg-config make ninja colcon-common-extensions catkin_tools rosdep
+
+# Install project dependencies
+pip install -r charge-port/requirements.txt 
+
+# Terminal 1: run talker
+mamba activate ros2_humble
+ros2 run demo_nodes_cpp talker
+
+# Terminal 2: run listener
+mamba activate ros2_humble
+ros2 run demo_nodes_py listener
